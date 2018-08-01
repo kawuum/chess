c8ba2768c96ca13bd9af723f72929572a87d243f
kawuum <>
2018-08-01 09:31:50 +0200

    Frame shows a simple chess field.

--- chess/gui/MainApp.cpp
+++ chess/gui/MainApp.cpp
@@ -4,13 +4,24 @@
 bool MainApp::OnInit()
 {
     wxInitAllImageHandlers();
-    wxBoxSizer* sizer = new wxBoxSizer(wxHORIZONTAL);
+    wxGridSizer* sizer = new wxGridSizer(8, 0, 0);
     MainFrame *frame = new MainFrame("Chess", wxPoint(50, 50), wxSize(640, 640));
+        
+    int color_counter = 0;
+    for(int i = 0; i < 8; ++i) {
+        for(int j = 0; j < 8; ++j) {
+            wxImagePanel *another_drawPane;
+            if(color_counter%2) {
+                another_drawPane = new wxImagePanel(frame, wxT("../gui/images/square_black.png"), 80, 80);
+            } else {
+                another_drawPane = new wxImagePanel(frame, wxT("../gui/images/square_white.png"), 80, 80);
+            }
+            sizer->Add(another_drawPane, 1, wxEXPAND);
+            ++color_counter;
+        }
+        ++color_counter;
+    }
     
-    drawPane = new wxImagePanel(frame, wxT("../gui/images/square_white.png"), 60, 60);
-    
-    drawPane->setRenderPosition(50, 50);
-    sizer->Add(drawPane, 1, wxEXPAND);
     frame->SetSizer(sizer);
     
     frame->Show(true);
