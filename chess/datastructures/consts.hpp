e89009df3e5ed20e3e2d9bf9b82efb9a4f0b326c
kawuum <>
2018-08-09 23:03:22 +0200

    Added const datastructures.

---
+++ chess/datastructures/consts.hpp
@@ -0,0 +1,11 @@
+#ifndef CONSTS_HPP
+#define CONSTS_HPP
+
+#include <wx/wx.h>
+
+const wxColor BG_GREY = wxColor(80,80,80);
+const wxColor BG_WHITE = wxColor(255, 255, 255);
+const wxColor BG_RED = wxColor(255, 0, 0);
+const wxColor BG_BLUE = wxColor(0, 0, 255);
+
+#endif
