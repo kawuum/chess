cf4bb46c0ebb6a47e3a5e4532b82c4393bbc52cc
cr4wler <marnap@gmx.de>
2018-10-24 14:42:58 +0200

    Some gui basics added

--- chess/qtgui/MainWindow.cpp
+++ chess/qtgui/MainWindow.cpp
@@ -2,10 +2,13 @@
 #include <QMenu>
 #include <QMenuBar>
 
-MainWindow::MainWindow(int* parent) : QMainWindow(parent)
+MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent)
 {
   QMenu *game = menuBar()->addMenu("&Game");
   QAction *new_game = new QAction("&New Game", this);
+  //new_game->setIcon(QIcon(":/images/new.png"));
+  //new_game->setShortcut(QKeySequence::New);
+  //new_game->setStatusTip(tr("Start a new game"));
   game->addAction(new_game);
   QAction *undo_move = new QAction("&Undo Move", this);
   game->addAction(undo_move);
@@ -13,7 +16,33 @@ MainWindow::MainWindow(int* parent) : QMainWindow(parent)
   game->addAction(add_ai);
   QAction *remove_ai = new QAction("&Remove AI", this);
   game->addAction(remove_ai);
+  game->addSeparator();
   QAction *quit = new QAction("&Quit", this);
   game->addAction(quit);
+  // TODO: Read and understand http://doc.qt.io/archives/qt-4.8/signalsandslots.html
+  //connect(new_game, SIGNAL(triggered()), this, SLOT(&MainWindow::new_game()));
+  //connect(undo_move, &QAction::triggered, this, MainWindow::undo_move);
+  //connect(add_ai, &QAction::triggered, this, MainWindow::add_ai);
+  //connect(remove_ai, &QAction::triggered, this, MainWindow::remove_ai);
   connect(quit, &QAction::triggered, qApp, QApplication::quit);
 }
+
+void MainWindow::new_game()
+{
+    qApp->quit();
+}
+
+void MainWindow::add_ai()
+{
+}
+
+void MainWindow::remove_ai()
+{
+}
+
+void MainWindow::undo_move()
+{
+}
+
+
+
--- chess/qtgui/MainWindow.h
+++ chess/qtgui/MainWindow.h
@@ -1,1 +1,18 @@
+#pragma once
 
+#include <QMainWindow>
+#include <QObject>
+#include <QApplication>
+
+class MainWindow : public QMainWindow {
+  Q_OBJECT
+  public:
+    MainWindow(QWidget *parent = 0);
+  public slots:
+    void new_game();
+    void undo_move();
+    void add_ai();
+    void remove_ai();
+  private:
+    
+};
